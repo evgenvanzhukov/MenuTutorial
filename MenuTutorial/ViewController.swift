2a2934d791c672a1cd4ae0e50cb477f12b8c6a01
evgen z <evgen-2208@ya.ru>
2023-01-07 10:58:06 +0400

    Initial Commit

---
+++ MenuTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MenuTutorial
+//
+//  Created by Evgen on 07.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
